cc96eb9166bfc9b2ab1fe184c724b85406307585
Manvendra <70734825+Ominous27@users.noreply.github.com>
2023-09-10 20:37:00 +0530

    Added the navbar entries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <header>
+      <div className="logo">
+        <img src='#' alt='Not Supported' />
+      </div>
+      <nav>
+        <ul>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#contact'>Contact</a></li>
+          <li><a href='#blogs'>Blogs</a></li>
+          <li><a href='#events'>Events</a></li>
+          <li><a href='#dashboard'>Dashboard</a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
