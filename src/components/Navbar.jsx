fb655a23bfadcef8829d7c98df65f127f481ab05
Manvendra <70734825+Ominous27@users.noreply.github.com>
2023-09-16 13:51:13 +0530

    Navbar for Desktop and Mobile view done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button';
 
 const Navbar = () => {
+  let links = [
+    {name:"HOME",link:"/"},
+    {name:"ABOUT US",link:"/"},
+    {name:"CONTACT",link:"/"},
+    {name:"EVENTS",link:"/"},
+    {name:"DASHBOARD",link:"/"},
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <header className='flex flex-row justify-between p-6'>
-      <nav className='flex justify-between items-center w-[92%] mx-auto'>
-
-        <div>
-          <img src='#' className='w-16' alt='...' />
-        </div>
-        
-        <div className='md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5'> 
-          <ul className='flex md:flex-row flex-col md:items-center md:gap-4 gap-8'>
-            <li><a className='hover:text-gray-500' href='#'>Home</a></li>
-            <li><a className='hover:text-gray-500' href='#'>Events</a></li>
-            <li><a className='hover:text-gray-500' href='#'>Dashboard</a></li>
-            <li><a className='hover:text-gray-500' href='#'>Blogs</a></li>
-            <li><a className='hover:text-gray-500' href='#'>Contact</a></li>
-          </ul>
-        </div>
-
-        <div>
-          <button className='bg-[#A6C1EE] text-white px-5 py-2 rounded-full hover:bg-[#87ACEC]'>Login</button>
-        </div>
-      </nav>
-    </header>
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <div className='font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] texxt-gray-800'>
+        <span className='text-3x1 text-indigo-600 mr-1 pt-2'>
+        <ion-icon name="logo-ionic"></ion-icon>
+        </span>
+        <img src='#' alt='...' />
+      </div>
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer  md:hidden'>
+        <ion-icon name="{open ? 'close':'menu'"></ion-icon>
+      </div>
+      <ul className={`md:flex md:items-center md:pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
+      ${open ? 'top-20':'top-[-490px]'}`}>
+        {
+          links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+            </li>
+          ))
+        }
+        <Button>
+          Login
+        </Button>
+      </ul>
+      </div>
+    </div>
   )
+
 }
 
 export default Navbar
