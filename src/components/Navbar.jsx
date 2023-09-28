84a69af84ebfeeeac81268caa9519bbbd1795e62
AbhayBhan <abhaybhan197@gmail.com>
2023-09-28 18:09:33 +0530

    Fix : Let changed to Const

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react'
 import Button from './Button';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 const Navbar = () => {
-  let links = [
+  const links = [
     {name:"HOME",link:"/"},
     {name:"ABOUT US",link:"/"},
     {name:"CONTACT",link:"/"},
     {name:"EVENTS",link:"/"},
     {name:"DASHBOARD",link:"/"},
   ];
-  let [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
       <div className='font-bold text-2x1 cursor-pointer flex items-center font-[Poppins] texxt-gray-800'>
         <span className='text-3x1 text-indigo-600 mr-1 pt-2'>
-        <ion-icon name="logo-ionic"></ion-icon>
         </span>
         <img src='#' alt='...' />
       </div>
       <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer  md:hidden'>
-        <ion-icon name="{open ? 'close':'menu'"></ion-icon>
+        <GiHamburgerMenu size={20} />
       </div>
       <ul className={`md:flex md:items-center md:pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
       ${open ? 'top-20':'top-[-490px]'}`}>
