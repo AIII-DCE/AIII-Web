08ad23f899b960e33daf6308dafa273e237763b1
AbhayBhan <abhaybhan197@gmail.com>
2023-09-30 20:01:31 +0530

    Fix : Fixed AboutUs Component Tree

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import IntroPage from '../components/IntroPage';
+import AboutUs from '../components/AboutUs';
 
 const Main = () => {
   return (
     <div className='block bg-white'>
         <IntroPage />
+        <AboutUs />
     </div>
   )
 }
